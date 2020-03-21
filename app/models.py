8ee3432abfffa046d71f5eec6d8eed0e9ac7268e
masonamccallum <masonamccallum@gmail.com>
2020-03-21 16:51:54 -0500

    3.21.20 update

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Game(db.Model):
 	__tablename__='games'
@@ -12,8 +14,21 @@ class Game(db.Model):
 class User(db.Model):
 	__tablename__='users'
 	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(64), unique=True, index=True)
 	name = db.Column(db.String(64), unique=True, index=True)
+	password_hash = db.Column(db.String(128))
 	game_id = db.Column(db.Integer, db.ForeignKey('games.id'))	
+
+	@property
+	def password(self):
+		raise AttributeError('password is not readable')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+		
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 	def __repr__(self):
 		return '<User %r>' % self.name
