a859c41f9d39764a0f10146305f3e4f5162004a4
masonamccallum <masonamccallum@gmail.com>
2020-04-01 14:08:53 -0500

    joinUserV1

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,3 +4,7 @@ from wtforms import *
 class newGameForm(FlaskForm):
 	rules = TextAreaField('Rules Here')#TODO make text file
 	submit = SubmitField('Create Game')
+
+class joinGameForm(FlaskForm):
+	code = StringField('Code',[validators.DataRequired()])
+	submit = SubmitField('Join Game')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, session, redirect, url_for
 from . import main
-from .forms import newGameForm
+from .forms import newGameForm, joinGameForm
 from .. import db
 from ..models import Game
+from flask_login import login_required
 
 
 @main.route('/')
@@ -10,11 +11,13 @@ def index():
 	return render_template('index.html')
 
 @main.route('/viewExistingGames')
+@login_required
 def viewGames():
 	print(Game.query.all())
 	return '<h1>viewGames</h1>'
 
 @main.route('/viewGameDetails/<game_id>',methods=['GET'])
+@login_required
 def viewGameInfo(game_id):
 	game = Game.query.filter_by(id=game_id).all()
 	if game:
@@ -25,12 +28,18 @@ def viewGameInfo(game_id):
 	return '<h1>viewing Game Information</h1>'
 
 @main.route('/createGame', methods=['GET','POST'])
+@login_required
 def createGame():
 	form = newGameForm()
-	db.create_all()
 	if form.validate_on_submit():
 		game = Game(rules=form.rules.data)
 		db.session.add(game)
 		db.session.commit()
 		return redirect('/viewExistingGames')
 	return render_template('createGame.html', form=form)
+	
+@main.route('/joinGame', methods=['GET','POST'])
+@login_required
+def joinGame():
+	form = joinGameForm()
+	return render_template('joinGame.html', form=form)
