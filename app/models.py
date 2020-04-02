19cd88717876a68161790ea9d3ae015e81e8a7c5
masonamccallum <masonamccallum@gmail.com>
2020-04-02 17:38:49 -0500

    new

--- app/models.py
+++ app/models.py
@@ -25,14 +25,11 @@ class User(UserMixin,db.Model):
 	@property
 	def password(self):
 		raise AttributeError('password is not readable')
-
 	@password.setter
 	def password(self, password):
 		self.password_hash = generate_password_hash(password)
-		
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
-
 	def __repr__(self):
 		return '<User %r>' % self.username
 
