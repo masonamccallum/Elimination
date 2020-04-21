93a36cad1962b14c295ff57241aab2984e642862
Rachael Brasovan <rachaelb@Rachaels-Beasty-MacBookPro.local>
2020-04-21 17:46:30 -0500

    create game page changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from wtforms import *
 from wtforms.validators import DataRequired
 
 class newGameForm(FlaskForm):
-	name= StringField('name',[validators.DataRequired()])
+	name= StringField('name',[validators.DataRequired()],render_kw = {"placeholder": "Title"})
 	ruleTitle = StringField('Title: ')
 	ruleBody = TextAreaField('Rule: ')
 	countdownLength = IntegerField('Days till start', [validators.DataRequired()])
