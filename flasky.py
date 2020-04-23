c34c373c446e48d408b7311c997ea2cf4f758c66
masonamccallum <masonamccallum@gmail.com>
2020-04-23 13:25:12 -0500

    test

--- flasky.py
+++ flasky.py
@@ -9,3 +9,9 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 @app.shell_context_processor
 def make_shell_context():
 	return dict(db=db, User=User, Game=Game, Role=Role, Permission=Permission, Rule=Rule, Poll=Poll)
+
+@app.cli.command()
+def test():
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	unittest.TextTestRunner(verbosity=2).run(tests)
