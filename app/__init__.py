14946aa7ab390e746d43f14958094f1439c08d4e
Chris Shipley <chris@aebok.com>
2020-04-23 00:39:33 +0000

    98% there for the create game page. Commiting for meeting, so latest code is on master branch. Completely stable. Just need to add custom rule functionality. Also did some code cleanup per my pylint

--- app/__init__.py
+++ app/__init__.py
@@ -10,21 +10,21 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
-	app = Flask(__name__)
-	login_manager.init_app(app)
-	app.config.from_object(config[config_name])
-	config[config_name].init_app(app)
-	bootstrap.init_app(app)
-	db.init_app(app)
+    app = Flask(__name__)
+    login_manager.init_app(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
 
-	from .main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
-	
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-	from .rules import rules as rules_blueprint
-	app.register_blueprint(rules_blueprint, url_prefix='/rules')
+    from .rules import rules as rules_blueprint
+    app.register_blueprint(rules_blueprint, url_prefix='/rules')
 
-	return app
+    return app
 
--- flasky.py
+++ flasky.py
@@ -1,11 +1,11 @@
 import os
 from app import create_app, db
 
-from app.models import User, Game, Role, Permission
+from app.models import User, Game, Role, Permission, Rule, Poll
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 @app.shell_context_processor
 def make_shell_context():
-	return dict(db=db, User=User, Game=Game, Role=Role, Permission=Permission)
+	return dict(db=db, User=User, Game=Game, Role=Role, Permission=Permission, Rule=Rule, Poll=Poll)
