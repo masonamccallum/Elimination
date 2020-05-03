cc76da5b1689043ee1cd02d92fdeabde5b7759af
Chris Shipley <chris@aebok.com>
2020-05-03 11:07:56 -0500

    Changed the migration back after testing - Need to finish migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
-from flask_migrate import Migrate
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -17,7 +16,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
-    migrate = Migrate(app, db)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
